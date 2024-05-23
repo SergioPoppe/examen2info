a494363ec6973a418c40650fa70f665b207b88cd
SergioPoppe <84764794+SergioPoppe@users.noreply.github.com>
2024-05-23 16:34:59 +0200

    Update index.js

--- src/views/codemasters/v1/asignaturas/index.js
+++ src/views/codemasters/v1/asignaturas/index.js
@@ -1,30 +1,5 @@
 'use strict'
 
 import { Router } from 'express'
-import { getProfesores, addProfesor } from './data.js';
-let router = Router()
-
-// Ruta para obtener todos los profesores
-router.get('/profesores', (req, res) => {
-    res.json(getProfesores());
-});
-
-// Ruta para añadir un nuevo profesor
-router.post('/profesores', (req, res) => {
-    const newProfesor = req.body;
-    addProfesor(newProfesor);
-    res.status(201).json(newProfesor);
-});
-
-// Ruta para obtener un profesor por ID
-router.get('/profesores/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const profesor = getProfesorById(id);
-    if (profesor) {
-        res.json(profesor);
-    } else {
-        res.status(404).send('Profesor no encontrado');
-    }
-});
 
 export default router
